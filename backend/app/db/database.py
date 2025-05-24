8a0d0f7edab04cfa850f421b2103aed07184f01a
AHKAYY007 <iamakwaubokemeh@gmail.com>
2025-05-24 04:59:35 -0700

    finale

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # Use async SQLite driver
 # SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./audit.db"
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = 'postgresql+asyncpg://avaxdb_user:c6VwIOfR4yxsRUXoWg7UAEuR1mkAJ3JO@dpg-d0or2lbe5dus73d5tgo0-a.oregon-postgres.render.com/avaxdb'
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, echo=True, future=True

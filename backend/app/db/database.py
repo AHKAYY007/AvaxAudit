085cf7a1960e088012f6361afc43f638f88daeb3
AHKAYY007 <iamakwaubokemeh@gmail.com>
2025-05-24 06:37:55 -0700

    finale

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -7,13 +7,15 @@ load_dotenv()
 
 # Use async SQLite driver
 # SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./audit.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 SQLALCHEMY_DATABASE_URL = 'postgresql+asyncpg://avaxdb_user:c6VwIOfR4yxsRUXoWg7UAEuR1mkAJ3JO@dpg-d0or2lbe5dus73d5tgo0-a.oregon-postgres.render.com/avaxdb'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://neondb_owner:npg_XjcEVRkxdw35@ep-red-hat-a84p9nf1-pooler.eastus2.azure.neon.tech/neondb?sslmode=require'
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, echo=True, future=True
 )
 
-AsyncSessionLocal = sessionmaker(
+AsyncSessionLocal = sessionmaker( 
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
 
